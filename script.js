4713e857f95f0d57f28592874585e9ecc346c554
Jason Gibson <71955479+jgibsone4@users.noreply.github.com>
2020-12-07 16:28:55 -0500

    working out array

--- script.js
+++ script.js
@@ -1,42 +1,97 @@
-//http://www.net-comber.com/charset.html
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
- var generateBtn = document.querySelector("#generate"); 
+function passwordLength() {
+  var length = 0;
 
-const resultEl = document.getElementById("password");
-const lengthEl = document.getElementById("length");
-const generateEl = document.getElementById("generate");
+  var alphabet = /[a-zA-Z]/g
 
-const randomFunc= {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomChar,
+  while (length < 8 || length > 128) {
+    length = prompt('Choose between 8 and 128 characters for your new password');
+
+  }
+
+  return length;
 }
+
+function charChoices() {
+  do {
+    
+    var charUpper = confirm("Include uppercase letters?");
+    var charLower = confirm("Include lowercase letters?");
+    var charNum = confirm("Include numbers?");
+    var charSpecial = confirm("Include special characters?");
+  
+
+  } while ((!charUpper && !charLower && !charNum && !charSpecial))
+
+  var uppercase = {
+    array: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    include: charUpper
+  }
+
+  var lowercase = {
+    array: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    include: charLower
+  }
+
+  var numbers = {
+    array: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    include: charNum
+  }
+
+  var special = {
+    array: ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+"],
+    include: charSpecial
+  }
+
+  var characters = [uppercase, lowercase, numbers, special];
+
+  var passArr;
+
+  
+  characters.forEach(type => {
+    if (!passArr && type.include) {
+      passArr = type.array;
+
+    } else if (passArr && type.include) {
+      passArr = passArr.concat(type.array);
+      console.log(passArr);
+    }
+  });
+
+  return passArr;
+}
+
+
+function randomNumber(length) {
+  return Math.floor(Math.random() * length);
+}
+
 
 function generatePassword() {
-  var passwordLength = prompt("Please enter the number of characters you want for your new password.  It must be more than 8 but less than 128.");
 
-  var numbers = confirm("Do you want numbers in your password?");
+  var password = "";
+  var length = passwordLength();
+  var passwordArr = charChoices();
 
-  var lowerCases = confirm("Do you want lowercases in your password?");
 
-  var upperCases = confirm("Do you want uppercases in your password?");
+  for (var i = 0; i < length; i++) {
+    var randomIndex = randomNumber(passwordArr.length);
+    password += passwordArr[randomIndex];
+  }
 
-  var special = confirm("Do you want special characters in your password?");
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  return password;
 }
 
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
 
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomChar() {
-  const char = "!@#$&()";
-  return char[Math.floor(Math.random()*char.length)];
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
